6ef9658a7fed84bad862b1d3085a251b1d2cbc1b
nastiakharchenko <60848688+nastiakharchenko@users.noreply.github.com>
2020-05-01 17:20:15 +0300

    Add files via upload

--- lab_8/js/get.js
+++ lab_8/js/get.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON('', function(jd) {
+    $.getJSON('https://cloud.iexapis.com/stable/stock/aapl/stats/latestPrice?', function(jd) {
           $('#stage').html('<p>Name: ' + jd.companyName + '</p>');
           $('#stage').append('<p>beta : ' + jd.beta+ '</p>');
           $('#stage').append('<p>divident: ' + jd.ttmDividendRate+ '</p>');
